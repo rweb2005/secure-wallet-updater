c962db4a8a678385475a72bbfc3c57b498bfac29
rweb2005 <realweb2005@gmail.com>
2023-05-22 14:00:00 +0200

    Add multi-sig wallet functionality

--- block_size_adjustment.go
+++ block_size_adjustment.go
@@ -1,3 +1,4 @@
 // Fix compatibility issues with MetaMask
 // Enhance decentralized autonomous governance
 // Add governance proposal expiration feature
+// Refactor decentralized governance structure
