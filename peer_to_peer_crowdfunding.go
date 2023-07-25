163789ba1461fbc0d02a5218f30176937a74ec3c
rweb2005 <realweb2005@gmail.com>
2023-07-25 14:00:00 +0200

    Add multi-sig wallet functionality

--- peer_to_peer_crowdfunding.go
+++ peer_to_peer_crowdfunding.go
@@ -1,3 +1,4 @@
 // Fix issue with multi-chain transactions
 // Add support for zk-SNARKs in smart contracts
 // Fix data consistency issues in smart contracts
+// Fix Oracle communication delay issues
