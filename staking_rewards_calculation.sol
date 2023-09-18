90c5b67356ce28c2b50402ca1b8185087aeb2505
rweb2005 <realweb2005@gmail.com>
2023-09-18 14:00:00 +0200

    Refactor staking logic

--- staking_rewards_calculation.sol
+++ staking_rewards_calculation.sol
@@ -1,3 +1,4 @@
 // Add governance proposal expiration feature
 // Integrate with decentralized identity providers
 // Integrate automated market maker (AMM) logic
+// Fix Oracle communication delay issues
