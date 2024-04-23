553d59a928cef5e7398d0fdf240047ff3a01c360
rweb2005 <realweb2005@gmail.com>
2024-04-23 14:00:02 +0200

    Improve DApp transaction flow

--- zk_rollups.sol
+++ zk_rollups.sol
@@ -1,3 +1,4 @@
 // Improve error handling in transaction flow
 // Improve staking pool reward management
 // Improve decentralized exchange (DEX) liquidity
+// Add support for DeFi lending protocols
