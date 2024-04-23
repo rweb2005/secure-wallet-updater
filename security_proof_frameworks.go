8230ff9d2ed3f21dea2d667b48bb6a31d05e3b13
rweb2005 <realweb2005@gmail.com>
2024-04-23 14:00:02 +0200

    Add slashing mechanism for malicious actors

--- security_proof_frameworks.go
+++ security_proof_frameworks.go
@@ -2,3 +2,4 @@
 // Optimize decentralized application performance
 // Refactor incentive structure in DeFi protocol
 // Fix wallet interaction issues in DeFi protocols
+// Refactor contract to use EIP-712 signatures
