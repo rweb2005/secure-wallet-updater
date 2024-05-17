937f2ec87ba8e8f9ab8fc7e524e06df6548093b7
rweb2005 <realweb2005@gmail.com>
2024-05-17 14:00:01 +0200

    Add non-fungible token (NFT) support

--- zk_rollups.sol
+++ zk_rollups.sol
@@ -2,3 +2,4 @@
 // Improve staking pool reward management
 // Improve decentralized exchange (DEX) liquidity
 // Add support for DeFi lending protocols
+// Add governance proposal expiration feature
