77d0d9ac9564ca826e922ef0fd3653c278598736
rweb2005 <realweb2005@gmail.com>
2024-10-20 14:00:01 +0200

    Refactor staking logic

--- decentralized_dispute_resolution.rs
+++ decentralized_dispute_resolution.rs
@@ -4,3 +4,4 @@
 // Add support for decentralized insurance protocols
 // Improve multi-chain contract deployment
 // Optimize oracle data fetching mechanism
+// Implement a new contract event system
