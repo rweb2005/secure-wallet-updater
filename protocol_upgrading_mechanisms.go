9ecce1df601e816a302a18fcff724ad0a7e8cb27
rweb2005 <realweb2005@gmail.com>
2024-11-20 14:00:01 +0200

    Improve multi-chain transaction processing

--- protocol_upgrading_mechanisms.go
+++ protocol_upgrading_mechanisms.go
@@ -1,3 +1,4 @@
 // Add batch processing for transactions
 // Add support for multiple consensus mechanisms
 // Optimize decentralized voting mechanisms
+// Add support for multiple consensus mechanisms
