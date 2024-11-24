ac827175d974c270a058cd85f3ded6edf977ad56
rweb2005 <realweb2005@gmail.com>
2024-11-24 14:00:01 +0200

    Optimize token minting flow

--- protocol_upgrading_mechanisms.go
+++ protocol_upgrading_mechanisms.go
@@ -2,3 +2,4 @@
 // Add support for multiple consensus mechanisms
 // Optimize decentralized voting mechanisms
 // Add support for multiple consensus mechanisms
+// Optimize cross-chain bridging mechanisms
