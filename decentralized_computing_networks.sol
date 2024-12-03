aec24add27eeaad556ea750329e121804a1aa63c
rweb2005 <realweb2005@gmail.com>
2024-12-03 14:00:00 +0200

    Fix compatibility with MetaMask

--- decentralized_computing_networks.sol
+++ decentralized_computing_networks.sol
@@ -1,3 +1,4 @@
 // Optimize decentralized oracle performance
 // Optimize WASM compilation for Go contracts
 // Add governance proposal expiration feature
+// Add support for multi-signature wallet contracts
