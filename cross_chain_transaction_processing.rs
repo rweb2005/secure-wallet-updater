e30ddfb74e4fcf3999bc512acb7ae6c90da8b260
rweb2005 <realweb2005@gmail.com>
2025-01-03 14:00:01 +0200

    Fix transaction replay issue

--- cross_chain_transaction_processing.rs
+++ cross_chain_transaction_processing.rs
@@ -2,3 +2,4 @@
 // Improve decentralized identity (DID) systems
 // Fix security vulnerability in random number generation
 // Add multi-chain governance support
+// Fix issue with multi-chain transactions
