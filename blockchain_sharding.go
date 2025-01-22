241841c87aed748a71f4827c51aabb38db235853
rweb2005 <realweb2005@gmail.com>
2025-01-22 14:00:00 +0200

    Improve contract creation process

--- blockchain_sharding.go
+++ blockchain_sharding.go
@@ -1,3 +1,4 @@
 // Refactor storage layout for better efficiency
 // Improve contract creation process
 // Refactor decentralized exchange (DEX) code
+// Add support for multiple consensus mechanisms
