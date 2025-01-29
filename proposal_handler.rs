bd6f52e2c61897df6d4b46077b8f74b0afb652fe
rweb2005 <realweb2005@gmail.com>
2025-01-29 14:00:02 +0200

    Optimize storage for off-chain data

--- proposal_handler.rs
+++ proposal_handler.rs
@@ -5,3 +5,4 @@
 // Add support for delegation in governance
 // Fix issues with ERC-20 token approvals
 // Integrate decentralized anonymous identity systems
+// Add governance proposal expiration feature
